87edc52c7d987f51ed08e5897eaa0f20f37282dc
mark405 <mark200320031.tun@gmail.com>
2022-10-11 12:50:18 +0300

    fix task1

--- lab2/task1.py
+++ lab2/task1.py
@@ -1,16 +1,18 @@
 class Rectangle:
-    def __init__(self):
-        self.__length = 1.
-        self.__width = 1.
+    def __init__(self, length=1, width=1):
+        self.__length = None
+        self.set_length(length)
+        self.__width = None
+        self.set_width(width)
 
     def set_length(self, length):
-        if isinstance(length, float | int) and 0 < length < 20:
+        if isinstance(length, (float, int)) and 0 < length < 20:
             self.__length = length
         else:
             raise ValueError
 
     def set_width(self, width):
-        if isinstance(width, float | int) and 0 < width < 20:
+        if isinstance(width, (float, int)) and 0 < width < 20:
             self.__width = width
         else:
             raise ValueError
@@ -22,9 +24,8 @@ class Rectangle:
         return self.__length * self.__width
 
 
-rectangle = Rectangle()
-
 try:
+    rectangle = Rectangle()
     rectangle.set_length(10)
     rectangle.set_width(2.5)
     print(rectangle.get_square())
