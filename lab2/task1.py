35483ff13c7ba136ac7894a4ef24506e7ce5b6a0
mark405 <mark200320031.tun@gmail.com>
2022-10-18 09:28:57 +0300

    Update task1

--- lab2/task1.py
+++ lab2/task1.py
@@ -1,8 +1,6 @@
 class Rectangle:
     def __init__(self, length=1, width=1):
-        self.__length = None
         self.set_length(length)
-        self.__width = None
         self.set_width(width)
 
     def set_length(self, length):
